b6b61251723951666b6e12869f91b46e729d85df
FranKR02 <frankdiego007@hotmail.com>
2023-11-18 22:01:35 -0500

    Configuración inicial

---
+++ src/main/java/projects/pizzeria/PizzeriaApplication.java
@@ -0,0 +1,13 @@
+package projects.pizzeria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzeriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzeriaApplication.class, args);
+	}
+
+}
