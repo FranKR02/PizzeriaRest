5be233fa88cd7d05c3b156af21e7e76a93dda69c
FranKR02 <frankdiego007@hotmail.com>
2023-11-19 16:56:48 -0500

    Se agregó el controller para las orednes

---
+++ src/main/java/projects/pizzeria/web/controller/OrderController.java
@@ -0,0 +1,32 @@
+package projects.pizzeria.web.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import projects.pizzeria.persistence.entity.PizzaOrder;
+import projects.pizzeria.service.OrderService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+    @GetMapping("/")
+    public ResponseEntity<List<PizzaOrder>> getAll(){
+        return ResponseEntity.ok(orderService.getAll());
+    }
+    @GetMapping("/today")
+    public ResponseEntity<List<PizzaOrder>> getTodayOrders(){
+        return ResponseEntity.ok(orderService.getTodayOrders());
+    }
+    @GetMapping("/outside")
+    public ResponseEntity<List<PizzaOrder>> getOutsideOrders(){
+        return ResponseEntity.ok(orderService.getOutsideOrders());
+    }
+}
