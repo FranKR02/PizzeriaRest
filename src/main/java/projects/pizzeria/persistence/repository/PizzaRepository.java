8db9daf46273c26f0a35a2922475396ece8b4396
FranKR02 <frankdiego007@hotmail.com>
2023-11-19 16:53:57 -0500

    Se modificó la interfaz PizzaRepository para añadirle query methods

--- src/main/java/projects/pizzeria/persistence/repository/PizzaRepository.java
+++ src/main/java/projects/pizzeria/persistence/repository/PizzaRepository.java
@@ -1,10 +1,19 @@
 package projects.pizzeria.persistence.repository;
 
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.stereotype.Repository;
 import projects.pizzeria.persistence.entity.Pizza;
 
 import java.util.List;
+import java.util.Optional;
 
+@Repository
 public interface PizzaRepository extends ListCrudRepository<Pizza, Long> {
     List<Pizza> findByAvailableFalse();
+    Optional<Pizza> findFirstByAvailableTrueAndNameIgnoreCase(String name);
+    List<Pizza> findAllByAvailableTrueAndDescriptionContainingIgnoreCase(String ingredient);
+    List<Pizza> findAllByAvailableTrueAndDescriptionNotContainsIgnoreCase(String ingredient);
+    List<Pizza> findTop3ByAvailableTrueAndPriceLessThanEqualOrderByPriceAsc(Double price);
+    Integer countByVeganTrue();
+
 }
