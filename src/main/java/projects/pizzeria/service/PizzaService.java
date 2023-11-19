844d7fbd66e9d50fe680a69aa14a3eaf7cf67c5e
FranKR02 <frankdiego007@hotmail.com>
2023-11-19 16:54:49 -0500

    Se modificó para usar los metodos de los repositorios

--- src/main/java/projects/pizzeria/service/PizzaService.java
+++ src/main/java/projects/pizzeria/service/PizzaService.java
@@ -1,9 +1,12 @@
 package projects.pizzeria.service;
 
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import projects.pizzeria.persistence.entity.Pizza;
+import projects.pizzeria.persistence.repository.PizzaPageSortRepository;
 import projects.pizzeria.persistence.repository.PizzaRepository;
 
 import java.util.List;
@@ -11,17 +14,47 @@ import java.util.List;
 @Service
 public class PizzaService {
     private final PizzaRepository pizzaRepository;
+    private final PizzaPageSortRepository pageSortRepository;
 
-    public PizzaService(PizzaRepository pizzaRepository) {
+    public PizzaService(PizzaRepository pizzaRepository, PizzaPageSortRepository pageSortRepository) {
         this.pizzaRepository = pizzaRepository;
+        this.pageSortRepository = pageSortRepository;
     }
-    public List<Pizza> getAll(){
-        return pizzaRepository.findAll();
+
+    public Page<Pizza> getAll(int page, int elements){
+        Pageable pageRequest = PageRequest.of(page, elements);
+        return pageSortRepository.findAll(pageRequest);
     }
-    public List<Pizza> getNotAvailables(){
-        return pizzaRepository.findByAvailableFalse();
+    public Page<Pizza> getAvailables(int page, int elements, String sortBy, String sortDirection){
+        Sort sort = Sort.by(Sort.Direction.fromString(sortDirection), sortBy);
+        Pageable pageRequest = PageRequest.of(page, elements, sort);
+        return pageSortRepository.findByAvailableTrue(pageRequest);
     }
     public Pizza getById(Long id){
         return pizzaRepository.findById(id).orElse(null);
+    }
+    public Pizza save(Pizza pizza){
+        return pizzaRepository.save(pizza);
+    }
+    public Boolean exist(Long id){
+        return pizzaRepository.existsById(id);
+    }
+    public void delete(Long id){
+        pizzaRepository.deleteById(id);
+    }
+    public Pizza getByName(String name){
+        return pizzaRepository.findFirstByAvailableTrueAndNameIgnoreCase(name).orElse(null);
+    }
+    public List<Pizza> containIngredient(String ingredient){
+        return pizzaRepository.findAllByAvailableTrueAndDescriptionContainingIgnoreCase(ingredient);
+    }
+    public List<Pizza> notContainIngredient(String ingredient){
+        return pizzaRepository.findAllByAvailableTrueAndDescriptionNotContainsIgnoreCase(ingredient);
+    }
+    public List<Pizza> getCheapest(Double price){
+        return pizzaRepository.findTop3ByAvailableTrueAndPriceLessThanEqualOrderByPriceAsc(price);
+    }
+    public Integer countVegan(){
+        return pizzaRepository.countByVeganTrue();
     }
 }
