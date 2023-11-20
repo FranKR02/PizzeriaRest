647cf2347782947e097385f12e34c4ac69ed91ab
FranKR02 <frankdiego007@hotmail.com>
2023-11-20 12:38:34 -0500

    Se agregaron los campos para auditar la creación y modificación de la entidad Pizza por medio del rol

--- src/main/java/projects/pizzeria/persistence/audit/Auditable.java
+++ src/main/java/projects/pizzeria/persistence/audit/Auditable.java
@@ -2,7 +2,9 @@ package projects.pizzeria.persistence.audit;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
@@ -15,4 +17,8 @@ public class Auditable {
     @Column(name = "modified_date")
     @LastModifiedDate
     private LocalDateTime modifiedDate;
+    @CreatedBy
+    private String createdBy;
+    @LastModifiedBy
+    private String modifiedBy;
 }
