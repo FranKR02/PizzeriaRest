6ab8f62e49c7afc61cc347df1052d4e8a049f67c
FranKR02 <frankdiego007@hotmail.com>
2023-11-21 13:00:57 -0500

    Creación de capas y terminación de borrado de archivos

--- src/main/java/projects/pizzeria/service/dto/LoginDTO.java
+++
@@ -1,9 +0,0 @@
-package projects.pizzeria.service.dto;
-
-import lombok.Data;
-
-@Data
-public class LoginDTO {
-    private String username;
-    private String password;
-}
--- src/main/java/projects/pizzeria/service/dto/UpdatePizzaPrieDTO.java
+++
@@ -1,9 +0,0 @@
-package projects.pizzeria.service.dto;
-
-import lombok.Data;
-
-@Data
-public class UpdatePizzaPrieDTO {
-    private Long pizzaId;
-    private Double newPrice;
-}
