03864bfedb77d6ba7bc3e286eac92640cb034bba
FranKR02 <frankdiego007@hotmail.com>
2023-11-21 12:54:28 -0500

    Reestructuración completa del proyecto

--- src/main/java/projects/pizzeria/persistence/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package projects.pizzeria.persistence.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import projects.pizzeria.persistence.entity.User;
-
-public interface UserRepository extends CrudRepository<User, String> {
-}
