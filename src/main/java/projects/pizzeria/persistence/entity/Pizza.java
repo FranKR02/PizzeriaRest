07ea2271b749e239c9a81fd3d3a09018d2500191
FranKR02 <frankdiego007@hotmail.com>
2023-11-21 20:08:09 -0500

    Se modificó la clase según las nuevas relaciones y el nombre de los campos para usar una sola convención

--- src/main/java/projects/pizzeria/persistence/entity/Pizza.java
+++ src/main/java/projects/pizzeria/persistence/entity/Pizza.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.annotations.ColumnDefault;
 
 import java.util.List;
 
@@ -15,20 +14,16 @@ import java.util.List;
 @Table(name = "pizzas")
 public class Pizza {
     @Id
-    @Column(length = 50)
-    private String name;
+    @Column(length = 50, unique = true)
+    private String nombre;
     @Column(length = 100, nullable = false)
-    private String description;
-    @Column(nullable = false, columnDefinition = "DECIMAL(2,5)")
-    private Double price;
+    private String descripcion;
+    @Column(nullable = false, columnDefinition = "DECIMAL(15,2)")
+    private Double precio;
     @Column(columnDefinition = "TINYINT DEFAULT 0")
-    private Boolean vegan;
+    private Boolean vegana;
     @Column(columnDefinition = "TINYINT DEFAULT 1")
-    private Boolean available;
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    @ColumnDefault("PERSONAL")
-    private Tamano tamano;
+    private Boolean disponible;
     @OneToMany(mappedBy = "id.idPizza", fetch = FetchType.EAGER)
     private List<PizzaIngrediente> ingredienteList;
 }
