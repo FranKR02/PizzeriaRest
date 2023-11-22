6a28a768fd793af6978dfaf6975d2b832ebd0491
FranKR02 <frankdiego007@hotmail.com>
2023-11-21 20:06:33 -0500

    Se agregaron anotaciones para tener un mejor control del nombre de los campos

--- src/main/java/projects/pizzeria/persistence/entity/PizzaIngredienteId.java
+++ src/main/java/projects/pizzeria/persistence/entity/PizzaIngredienteId.java
@@ -1,14 +1,24 @@
 package projects.pizzeria.persistence.entity;
 
 import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.io.Serializable;
-
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode
 @Embeddable
 public class PizzaIngredienteId implements Serializable {
     @ManyToOne
+    @JoinColumn(name = "id_pizza")
     private Pizza idPizza;
     @ManyToOne
+    @JoinColumn(name = "id_ingrediente")
     private Ingrediente idIngrediente;
 }
