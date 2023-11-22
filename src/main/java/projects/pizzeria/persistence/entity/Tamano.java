c4a0ffabbb9042f988a8cf82a83063803ba4204e
FranKR02 <frankdiego007@hotmail.com>
2023-11-21 20:05:43 -0500

    Se cambio de enum a class

--- src/main/java/projects/pizzeria/persistence/entity/Tamano.java
+++ src/main/java/projects/pizzeria/persistence/entity/Tamano.java
@@ -1,8 +1,17 @@
 package projects.pizzeria.persistence.entity;
 
-public enum Tamano {
-    PERSONAL,
-    MEDIANA,
-    GRANDE,
-    EXTRA_GRANDE
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "tamanos")
+public class Tamano {
+    @Id
+    @Column(updatable = false)
+    private String tamano;
 }
